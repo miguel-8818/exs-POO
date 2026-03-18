42a21b7de3fdafbddffb4401c1ba9d1af38d6373
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:25:25 -0300

    Converter Celsius para Fahrenheit.

---
+++ 2ds-atividades/atv5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperatura graus = new Temperatura();
+
+            Console.Write("Digite a temperatura em Celsius: ");
+            graus.Celsius = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = graus.Converter();
+
+            Console.WriteLine("emperatura em Fahrenheit: " + resultado);
+        }
+    }
+}
