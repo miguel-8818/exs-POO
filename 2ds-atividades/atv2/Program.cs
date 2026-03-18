5a9e5df8c3ae3b72bc0c3a30c56544c2b1fc98fc
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:16:47 -0300

    Solicitar Nome

---
+++ 2ds-atividades/atv2/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Solicitacao nome = new Solicitacao();
+
+            Console.WriteLine("Digite seu Nome: ");
+            nome.nome = Console.ReadLine();
+
+            nome.ExibirSolicitacao();
+        }
+    }
+}
