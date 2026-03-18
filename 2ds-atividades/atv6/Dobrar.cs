0669b8e8cc57dc5d5e3e29534214033e2ad7c228
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:29:20 -0300

    Classe Dobrar.

---
+++ 2ds-atividades/atv6/Dobrar.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv6
+{
+    internal class Dobrar
+    {
+        public double Valor { get; set; }
+
+
+        public double Dobro()
+        {
+            return Valor * 2;
+        }
+    }
+}
