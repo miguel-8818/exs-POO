50e5be771b2bfb915161fc7b1805c08675c5e47b
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:07:04 -0300

    Mensagem "Olá, Mundo!".

---
+++ 2ds-atividades/atv1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Mensagem mensagem = new Mensagem();
+            mensagem.msg = "Olá, Mundo!";
+            mensagem.ExibirMensagem();
+        }
+    }
+}
