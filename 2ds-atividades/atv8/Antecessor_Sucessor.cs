bf5afdb6abf3c5624fbf1db65a9d2dbe5dfe7750
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:33:01 -0300

    Classe Antecessor_Sucessor.

---
+++ 2ds-atividades/atv8/Antecessor_Sucessor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv8
+{
+    internal class Antecessor_Sucessor
+    {
+        public int Numero { get; set; }
+        public string valor;
+
+        public int Antecessor()
+        {
+            return Numero - 1;
+        }
+        public int Sucessor()
+        {
+            return Numero + 1;
+        }
+    }
+}
