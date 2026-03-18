02dcde7433f53344fa4904d2af519ba3e21051ad
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:19:01 -0300

    Soma de dois Numeros.

---
+++ 2ds-atividades/atv3/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora soma = new Calculadora();
+
+            Console.Write("Digite o primeiro número: ");
+            soma.num1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo número: ");
+            soma.num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Resultado: " + soma.Calcular());
+        }
+    }
+}
