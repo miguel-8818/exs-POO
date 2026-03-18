c81feb21c9c3549ea8f8962dd5b80418b4e40011
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:14:41 -0300

    Classe "Mensagem"

---
+++ 2ds-atividades/atv1/Mensagem.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv1
+{
+    internal class Mensagem
+    {
+        public string msg;
+
+    public void ExibirMensagem()
+    {
+        Console.WriteLine(msg);
+    }
+    }
+}
