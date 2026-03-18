a13dab2ed8b70fd9fb6878291bd7086e86647a56
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:30:07 -0300

    Converter Dolar para Real.

---
+++ 2ds-atividades/atv7/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Conversor conversor = new Conversor();
+
+            Console.Write("Digite o valor em Dólar: ");
+            conversor.Dolar = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Valor em Reais: R$ " + conversor.Converter());
+        }
+    }
+}
