297185b1e2339f1dd5613f29cc3ec05544a6957d
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:36:30 -0300

    Classe Leitor.

---
+++ 2ds-atividades/atv9/Leitor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv9
+{
+    internal class Leitor
+    {
+        public string Nome { get; set; }
+        public int Idade { get; set; }
+
+        public string Mensagem()
+        {
+            return $"Bem-vindo, {Nome}! Você tem {Idade} anos!.";
+        }
+
+    }
+}
