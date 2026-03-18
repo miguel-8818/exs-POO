c5908c108dfe212ebd918522f21a45f0a49f747f
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:35:59 -0300

    Solicitar um Nome e umadade, e depois exibir uma Mensagem.

---
+++ 2ds-atividades/atv9/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Leitor leitor = new Leitor();
+
+            Console.Write("Digite seu nome: ");
+            leitor.Nome = Console.ReadLine();
+
+            Console.Write("Digite sua idade: ");
+            leitor.Idade = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine(leitor.Mensagem());
+        }
+    }
+}
