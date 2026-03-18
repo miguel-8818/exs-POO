8aa3816492e09bc9fcc20d1b501912e8aaa23112
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:19:42 -0300

    Classe Calculadora.

---
+++ 2ds-atividades/atv3/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv3
+{
+    internal class Calculadora
+    {
+        public int num1;
+        public int num2;
+        public string soma;
+
+        public int Calcular()
+        {
+            return num1 + num2;
+        }
+    }
+}
