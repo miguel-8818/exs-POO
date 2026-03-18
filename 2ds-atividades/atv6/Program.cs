12bf8a6bd9cae102b4aa6874b5c3d7c9553244b0
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:28:48 -0300

    Calcular o Dobro de um Numero.

---
+++ 2ds-atividades/atv6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dobrar numero = new Dobrar();
+
+            Console.Write("Digite um número: ");
+            numero.Valor = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("O dobro é: " + numero.Dobro());
+        }
+    }
+}
