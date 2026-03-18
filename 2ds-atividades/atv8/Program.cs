e7d9af9b23e94281f633f137df163d90e285100a
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:32:12 -0300

    Solicitar um Numero e mostrar seu Antecessor e Sucessor.

---
+++ 2ds-atividades/atv8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Antecessor_Sucessor valor = new Antecessor_Sucessor();
+
+            Console.Write("Digite um número inteiro: ");
+            valor.Numero = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Antecessor: " + valor.Antecessor());
+            Console.WriteLine("Sucessor: " + valor.Sucessor());
+        }
+    }
+}
