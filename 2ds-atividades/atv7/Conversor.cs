86c0924cb9b22366c8fd8b97c392b689e752e138
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:30:37 -0300

    Classe Conversor.

---
+++ 2ds-atividades/atv7/Conversor.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv7
+{
+    internal class Conversor
+    {
+        public double Dolar;
+        public double Taxa = 5.0;
+
+        public double Converter()
+        {
+            return Dolar * Taxa;
+        }
+    }
+}
