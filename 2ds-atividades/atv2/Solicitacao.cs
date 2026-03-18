9ac1bd7d5b76990c635a91949a7923cfde71e6f0
miguel.fereira <miguel.ferreira59etcjk@gmail.com>
2026-03-18 12:17:17 -0300

    Classe Solicitacao

---
+++ 2ds-atividades/atv2/Solicitacao.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace atv2
+{
+    internal class Solicitacao
+    {
+        public string nome;
+
+        public void ExibirSolicitacao()
+        {
+            Console.WriteLine("Olá, " + nome + "!");
+        }
+    }
+}
